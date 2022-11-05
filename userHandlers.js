035210ecd4d115c48eed8fbf72af017c07cf1c97
Quentin Feuvrais <feuvrais.quentin@gmail.com>
2022-11-05 16:41:21 +0100

    doing Express 03

--- userHandlers.js
+++ userHandlers.js
@@ -1,5 +1,4 @@
 const database = require("./database");
-
 const getUsers = (req, res) => {
   database
     .query("select * from users")
@@ -30,7 +29,25 @@ const getUsersByID = (req, res) => {
     });
 };
 
+const postUser = (req, res) => {
+  const { firstname, lastname, email, city, language } = req.body;
+
+  database
+    .query(
+      "INSERT INTO users (firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
+      [firstname, lastname, email, city, language]
+    )
+    .then(([result]) => {
+      res.location(`/api/users/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error saving the user");
+    });
+};
+
 module.exports = {
   getUsers,
   getUsersByID,
+  postUser,
 };
