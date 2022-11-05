fee2b952c66d9a7680b2ace5eb53612bb5544823
Quentin Feuvrais <feuvrais.quentin@gmail.com>
2022-11-05 18:10:26 +0100

    doing Express 05

--- app.js
+++ app.js
@@ -23,14 +23,18 @@ app.post("/api/movies", movieHandlers.postMovie);
 
 app.put("/api/movies/:id", movieHandlers.updateMovie);
 
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+
 const userHandlers = require("./userHandlers");
 
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUsersByID);
 
-app.post("/api/users", userHandlers.postUser)
+app.post("/api/users", userHandlers.postUser);
 
-app.put("/api/users/:id", userHandlers.updateUsers)
+app.put("/api/users/:id", userHandlers.updateUsers);
+
+app.delete("/api/users/:id", userHandlers.deleteUser);
 
 app.listen(port, (err) => {
   if (err) {
